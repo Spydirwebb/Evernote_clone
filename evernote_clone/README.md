d9bbd0bbe18261698b201932bce76a87f342aca6
Spydirwebb <webb.jeffery.89@gmail.com>
2021-03-11 07:01:20 +0000

    navbar_built

---
+++ evernote_clone/README.md
@@ -0,0 +1,7 @@
+Evernote Clone from Udemy React Course
+
+keywords: react, redux, firebase, materialize
+
+#outline instructions (to test your skills if without the full walkthrough) 
+  1)Navbar with 1 Link - favorites
+    
---
+++ evernote_clone/src/App.js
@@ -0,0 +1,13 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import Navbar from './components/layout/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <Router>
+        <Navbar />
+    </Router>
+  );
+}
+
+export default App;
