d08594aa8e0a1510d5707de367f464fa5d6ce873
Spydirwebb <webb.jeffery.89@gmail.com>
2021-04-30 19:44:27 +0000

    styled sign in

--- evernote_clone/src/routes/SignIn.jsx
+++ evernote_clone/src/routes/SignIn.jsx
@@ -17,16 +17,26 @@ const SignIn = () => {
             });
     };
     return (
-        <div>
-            <h1>Sign In</h1>
-            <button
-                onClick={(event) => {
-                    event.preventDefault();
-                    signInWithGoogle();
-                }}
-            >
-                Sign in with Google
-            </button>
+        <div className="container">
+        <div className="row">
+            <div className="col s12 m4">
+                <div className="card green darken-2"> 
+                    <div className="card-content white-text">
+                        <span className="card-title">Sign In</span>
+                    </div>
+                    <div className="card-action">
+                        <button
+                            onClick={(event) => {
+                            event.preventDefault();
+                            signInWithGoogle();
+                            }}
+                            className="waves-effect waves-light btn social google">
+                        Sign in with Google
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     );
 };
